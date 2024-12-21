da5ff609ef25d9fd185cb1e8c34a7bac0a568ebd
Rahul <rajamohanty12345@gmail.com>
2024-12-21 18:57:16 +0530

    chore: fixed route in src

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import PropertyPage from "./pages/PropertyPage";
 import PropertyDetailPage from "./pages/PropertyDetailPage";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { WishListContextProvider } from "./context/WishListContext";
+
 const App = () => {
   return (
     <>
       <WishListContextProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/property" element={<PropertyPage />} />
+            <Route path="/" element={<PropertyPage />} />
             <Route path="/property/:id" element={<PropertyDetailPage />} />
           </Routes>
         </BrowserRouter>
